7ae62cc9522e94d6ac7bdca2604576bc911d2b4e
AnderVicario <102811194+AnderVicario@users.noreply.github.com>
2023-09-15 15:08:01 +0200

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -1,1 +1,30 @@
+// Saioa hasteko datuak egiaztatzeko funtzioa
+function ahalBadaSaioaSartu(erabiltzailea, pasahitza) {
+    // Erabiltzaile izenaren eta pasahitzaren zuzentasuna egiaztatzen du
+    if (erabiltzailea === "admin" && pasahitza === "admin") {
+        return true; // Saioa hasi da
+    }
+    else {
+        return false; // Erabiltzaile izena edo pasahitza ez da zuzena
+    }
+}
 
+// Formularioa bidaltzeko funtzioa
+function saioanSartu() {
+
+    // Formularioko erabiltzaile izena eta pasahitza lortzen ditu
+    const erabiltzailea = document.getElementById("erabiltzailea").value;
+    const pasahitza = document.getElementById("pasahitza").value;
+
+    // Saioa egiaztatzen du
+    if (ahalBadaSaioaSartu(erabiltzailea, pasahitza)) {
+        window.location.href = "main.html";
+    }
+    else {
+        alert("Ez zara saioan sartu");
+    }
+}
+
+// Formularioa lortu eta bidalketa ekitaldi bat gehitu
+var login_botoia = document.getElementById("login_botoia");
+login_botoia.addEventListener("click", saioanSartu);
