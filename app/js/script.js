f0d83b37a632f729a6dcc60cd55936ec6dee392f
Markel15 <103565976+Markel15@users.noreply.github.com>
2023-09-24 03:17:14 +0200

    izena emateko script hedatu

--- app/js/script.js
+++ app/js/script.js
@@ -19,7 +19,7 @@ function bordeGorriaIpiniKendu(ipini) {
 // Saioa hasteko datuak egiaztatzeko funtzioa
 function hutsikDago(erabiltzailea, pasahitza) {
     // Erabiltzaile izenaren eta pasahitzaren zuzentasuna egiaztatzen du
-    
+   
     if (erabiltzailea.trim() === "" || pasahitza.trim() === "" ) {
         // Hutsunea hutsik dago, borde gorria ipini
         console.log("1");
@@ -67,9 +67,54 @@ function saioanSartu() {
         //Ez duzu erabiltzailea edo pasahitza sartu
     }
 }
+function iEmanOrrira(){
+	event.preventDefault();
+	window.location.href = "sign.html";
+}
+
+function izenaEman(){
+  var data = Date.parse(document.getElementById("JaioData").value.toString());
+  var emaitza=true;
+  if(isNaN(data)){//konprobatu sartutako data baliozkoa den ala ez
+    alert("Ezarritako datak ez du balio");
+    emaitza=false;
+  }
+  if(nanKonprobatu()==false){
+  alert("Idatzitako NAN-a ez da egokia, hizkiak ez du zenbakiekin bat egiten");
+    emaitza=false;
+  }
+  if(emaitza==false){//Ez badira baldintzak betetzen ez bidali informazioa
+    event.preventDefault();
+  }
+}
+function nanKonprobatu(){
+  var nan=document.getElementById("NAN").value.toString();
+  var zenb=nan.substring(0,8);
+  var hizkia=nan.substring(9,10);
+  var kode="TRWAGMYFPDXBNJZSQVHLCKE";
+  var emaitza=kode.charAt((zenb % 23)-1);
+  if(emaitza!=hizkia){
+  return false;
+  }
+  else{
+  return true;
+  }
+}
+
 
 // Formularioa lortu eta bidalketa ekitaldi bat gehitu
 const erabiltzailea_hutsune = document.getElementById("erabiltzailea");
 const pasahitza_hutsune = document.getElementById("pasahitza");
 var login_botoia = document.getElementById("login_botoia");
-login_botoia.addEventListener("click", saioanSartu);
+if (login_botoia) {
+  // Konprobatu elementua existitzen dela orri honetan, bestela errorea ematen du
+  login_botoia.addEventListener("click", saioanSartu);
+}
+var signup_botoia = document.getElementById("signup_botoia");
+if (signup_botoia) {
+  signup_botoia.addEventListener("click", iEmanOrrira);
+}
+var sign_botoia=document.getElementById("iz_em_bot");
+if(sign_botoia){
+  sign_botoia.addEventListener("click",izenaEman);
+}
