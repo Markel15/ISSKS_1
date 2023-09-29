b1f7551d6b42ea450d358cea6ad42d62e5597af7
Markel15 <103565976+Markel15@users.noreply.github.com>
2023-09-29 13:45:26 +0200

    database updated

--- database.sql
+++ database.sql
@@ -1,11 +1,11 @@
 -- phpMyAdmin SQL Dump
--- version 5.0.2
+-- version 5.2.1
 -- https://www.phpmyadmin.net/
 --
 -- Servidor: db
--- Tiempo de generación: 16-09-2020 a las 16:37:17
--- Versión del servidor: 10.5.5-MariaDB-1:10.5.5+maria~focal
--- Versión de PHP: 7.4.9
+-- Tiempo de generación: 27-09-2023 a las 20:30:42
+-- Versión del servidor: 10.8.2-MariaDB-1:10.8.2+maria~focal
+-- Versión de PHP: 8.2.8
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
@@ -24,29 +24,51 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
--- Estructura de tabla para la tabla `usuarios`
+-- Estructura de tabla para la tabla `ERABILTZAILEA`
 --
 
-CREATE TABLE ERABILTZAILEA (
-  Izena VARCHAR(20) NOT NULL,
-  Pasahitza VARCHAR(20) NOT NULL,
-  PRIMARY KEY (Izena)
+CREATE TABLE `ERABILTZAILEA` (
+  `Izena` varchar(20) NOT NULL,
+  `Pasahitza` varchar(20) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
--- Volcado de datos para la tabla `usuarios`
+-- Volcado de datos para la tabla `ERABILTZAILEA`
 --
 
-INSERT INTO ERABILTZAILEA VALUES("ANDER", "ANDER");
-COMMIT;
+INSERT INTO `ERABILTZAILEA` (`Izena`, `Pasahitza`) VALUES
+('ANDER', 'ANDER');
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `LIBURUA`
+--
+
+CREATE TABLE `LIBURUA` (
+  `Titulua` varchar(20) NOT NULL,
+  `Autorea` varchar(20) NOT NULL,
+  `Generoa` varchar(20) NOT NULL,
+  `Prezioa` float NOT NULL,
+  `ISBN` int(15) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
 -- Índices para tablas volcadas
 --
 
 --
--- Indices de la tabla `usuarios`
+-- Indices de la tabla `ERABILTZAILEA`
 --
+ALTER TABLE `ERABILTZAILEA`
+  ADD PRIMARY KEY (`Izena`);
+
+--
+-- Indices de la tabla `LIBURUA`
+--
+ALTER TABLE `LIBURUA`
+  ADD PRIMARY KEY (`ISBN`);
+COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
